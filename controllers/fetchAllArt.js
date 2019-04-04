a5e142ba873807f8911fb2cd7b938c8ed61218c3
Chad Hamre <chadhamre@gmail.com>
2019-04-04 13:44:20 -0700

    chore: add to docs

--- controllers/fetchAllArt.js
+++ controllers/fetchAllArt.js
@@ -3,37 +3,42 @@ const pool = require("../database/mysql");
 
 // controller
 fetchAllArt = async ctx => {
-  // TODO: add pagination, for now limiting to first 100 records
-  const query =
-    "select t1.*, u.name from (select a.id as artID, a.title, a.artist, c.content, c.userID from art as a left join comments as c on a.id = c.artID limit 100) as t1 left join users as u on t1.userID = u.id";
-  const results = await pool.query(query);
+  try {
+    // TODO: add pagination, for now limiting to first 100 records
+    // execute query
+    const query =
+      "select t1.*, u.name from (select a.id as artID, a.title, a.artist, c.content, c.userID from art as a left join comments as c on a.id = c.artID limit 100) as t1 left join users as u on t1.userID = u.id";
+    const results = await pool.query(query);
 
-  let output = {};
-  results.forEach(row => {
-    if (!output[row.artID]) {
-      output[row.artID] = {
-        id: row.artID,
-        title: row.title,
-        artist: row.artist,
-        year: row.year,
-        comments: []
-      };
-    }
-    if (row.content) {
-      output[row.artID].comments.push({
-        id: row.id,
-        content: row.content,
-        name: row.name,
-        userID: row.userID
-      });
-    }
-  });
+    let output = {};
+    results.forEach(row => {
+      if (!output[row.artID]) {
+        output[row.artID] = {
+          id: row.artID,
+          title: row.title,
+          artist: row.artist,
+          year: row.year,
+          comments: []
+        };
+      }
+      if (row.content) {
+        output[row.artID].comments.push({
+          id: row.id,
+          content: row.content,
+          name: row.name,
+          userID: row.userID
+        });
+      }
+    });
 
-  let response = Object.keys(output).map(key => {
-    return output[key];
-  });
+    let response = Object.keys(output).map(key => {
+      return output[key];
+    });
 
-  ctx.body = response;
+    ctx.body = response;
+  } catch (err) {
+    console.log("ERROR", err);
+  }
 };
 
 module.exports = fetchAllArt;
