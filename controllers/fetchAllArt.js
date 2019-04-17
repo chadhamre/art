7151c91a9509a3f64c89fb0f67021af8cd7699a6
Chad Hamre <chadhamre@gmail.com>
2019-04-17 16:46:34 -0700

    feat: add bids to art endpoint

--- controllers/fetchAllArt.js
+++ controllers/fetchAllArt.js
@@ -4,14 +4,12 @@ const pool = require("../database/mysql");
 // controller
 fetchAllArt = async ctx => {
   try {
-
     // execute query
     const query =
-      "select t1.*, u.name from (select a.id as artID, a.title, a.artist, c.content,  c.name as cName, c.userID from art as a left join comments as c on a.id = c.artID limit 100) as t1 left join users as u on t1.userID = u.id";
+      "select t2.*, b.* from (select t1.*, u.name from (select a.id as artID, a.title, a.artist, a.status, c.content,  c.name as cName, c.userID from art as a left join comments as c on a.id = c.artID limit 100) as t1 left join users as u on t1.userID = u.id) as t2  left join bids as b on t2.artID = b.artID";
     const results = await pool.query(query);
 
     // TODO: add pagination, for now limiting to first 100 records
-
 
     // format response
     let output = {};
@@ -22,15 +20,26 @@ fetchAllArt = async ctx => {
           title: row.title,
           artist: row.artist,
           year: row.year,
-          comments: []
+          comments: [],
+          status: row.status,
+          bids: []
         };
       }
       if (row.content) {
         output[row.artID].comments.push({
           id: row.id,
           content: row.content,
-          name: !row.name? row.cName : row.name,
+          name: !row.name ? row.cName : row.name,
           userID: row.userID
+        });
+      }
+      if (row.round) {
+        output[row.artID].bids.push({
+          userID: row.userId,
+          name: row.name,
+          randNum: row.random,
+          bidInc: row.increment,
+          amount: row.bid
         });
       }
     });
@@ -41,7 +50,6 @@ fetchAllArt = async ctx => {
 
     // return response
     ctx.body = response;
-
   } catch (err) {
     console.log("ERROR", err);
   }
--- controllers/fetchArtById.js
+++ controllers/fetchArtById.js
@@ -4,9 +4,10 @@ const pool = require("../database/mysql");
 // controller
 fetchAllArtById = async ctx => {
   try {
-
     // execute query
-    const query = `select t1.*, u.name from (select a.id as artID, a.title, a.artist, c.content, c.name as cName, c.userID from art as a left join comments as c on a.id = c.artID where a.id = ${ctx.params.id}) as t1 left join users as u on t1.userID = u.id`;
+    const query = `select t2.*, b.* from (select t1.*, u.name from (select a.id as artID, a.title, a.artist, c.content, c.name as cName, c.userID from art as a left join comments as c on a.id = c.artID where a.id = ${
+      ctx.params.id
+    }) as t1 left join users as u on t1.userID = u.id) as t2 left join bids as b on t2.artID = b.artID;`;
     const results = await pool.query(query);
     let output = {};
 
@@ -18,15 +19,26 @@ fetchAllArtById = async ctx => {
           title: row.title,
           artist: row.artist,
           year: row.year,
-          comments: []
+          comments: [],
+          status: row.status,
+          bids: []
         };
       }
       if (row.content) {
         output[row.artID].comments.push({
           id: row.id,
           content: row.content,
-          name: !row.name? row.cName : row.name,
+          name: !row.name ? row.cName : row.name,
           userID: row.userID
+        });
+      }
+      if (row.round) {
+        output[row.artID].bids.push({
+          userID: row.userId,
+          name: row.name,
+          randNum: row.random,
+          bidInc: row.increment,
+          amount: row.bid
         });
       }
     });
